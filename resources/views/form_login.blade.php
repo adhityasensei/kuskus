43e2c356984bf1e126230dc6f879595513ede373
franciskus dhemas angger <franciskus.dhemas@pegadaian.co.id>
2022-06-09 22:19:11 +0700

    upload migration

---
+++ resources/views/form_login.blade.php
@@ -0,0 +1,8 @@
+<form method="post" name="loginform" action="{{ url('login') }}">
+    @csrf
+    <input type="text" placeholder="Your E-Mail" name="email" id="inputEmail">
+    <br><br>
+    <input type="password" placeholder="Your password" name="password" id="inputEmail">
+    <br><br>
+    <input type="submit" value="Login">
+</form>
